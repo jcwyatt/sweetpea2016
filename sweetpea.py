518950eb2b01c7428606271bb6bb71b839779b1c
jcwyatt <jwyatt043@googlemail.com>
2016-04-21 16:39:43 +0100

    Update sweetpea.py
    Added comment about the error in line 8

--- sweetpea.py
+++ sweetpea.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 def wait():
     # Calculate the delay to the start of the next hour
     next_hour = (datetime.now() + timedelta(hours=1)).replace(
-        minute=0, second=0, microsecond=0)
+        minute=0, second=0, microsecond=0)   #in the original code online, hours=1 was hour=1 and caused an error.
     delay = (next_hour - datetime.now()).seconds
     time.sleep(delay)
 
